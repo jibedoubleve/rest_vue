ea3a2372cb539cb528383d5f8130ff60af429fc1
jibedoubleve <jibedoubleve@users.noreply.github.com>
2021-10-07 16:16:04 +0200

    (wip) Work with API

---
+++ src/js/app8.js
@@ -0,0 +1,33 @@
+var app8 = new Vue({
+    el: "#app-8",
+    data: {
+        lines: "",
+        loadStatus: ""
+    },
+    methods: {
+        fetch: function () {
+            // Create a request variable and assign a new XMLHttpRequest object to it.
+            var request = new XMLHttpRequest();
+
+            // Open a new connection, using the GET request on the URL endpoint
+            request.open('GET', 'http://apex-acc/ords/wadb/api/v1/MAINTENANCE_CONTRACT', true);
+
+            request.onload = function () {
+                // Begin accessing JSON data here
+
+                this.status = "Almost..."
+                var data = JSON.parse(this.response)
+
+                this.lines = data.items;
+                this.loadStatus = "Work done";
+                console.info("Work done.");
+            };
+
+            // Send request
+            request.send();
+
+            this.status = "Work in progress...";
+
+        }
+    }
+});
