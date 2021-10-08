0ba742f31d3e3917913d81534d04ff5bf10eca73
jibedoubleve <jibedoubleve@gmail.com>
2021-10-08 13:28:10 +0200

    (WIP) load rest service

--- src/js/app8.js
+++ src/js/app8.js
@@ -2,30 +2,31 @@ var app8 = new Vue({
     el: "#app-8",
     data: {
         lines: "",
-        loadStatus: ""
+        loadStatus: "",
+        pageNr: "1",
+        buttonText: "Fetch",
+        url: 'https://reqres.in/api/users'
     },
     methods: {
         fetch: function () {
             this.loadStatus = "Work in progress...";
-            // Create a request variable and assign a new XMLHttpRequest object to it.
-            var request = new XMLHttpRequest();
 
-            // Open a new connection, using the GET request on the URL endpoint
-            request.open('GET', 'http://apex-acc/ords/wadb/api/v1/MAINTENANCE_CONTRACT', true);
+            const current_url = (this.pageNr != -1) ? this.url + "?page=" + this.pageNr : this.url;
+            axios
+                .get(current_url)
+                    .then(response => {
+                        this.lines = response.data;
+                        this.loadStatus = "Page " + this.pageNr + "/" + response.data.total_pages + " loaded...";
 
-            request.onload = function () {
-                // Begin accessing JSON data here
-
-                this.loadStatus = "Almost..."
-                var data = JSON.parse(this.response)
-
-                this.lines = data.items;
-                this.loadStatus = "Work done";
-                console.info("Work done.");
-            };
-
-            // Send request
-            request.send();
+                        if (this.pageNr < response.data.total_pages) {
+                            this.pageNr++;
+                            this.buttonText = "Fetch page " + this.pageNr;
+                        }
+                        else {
+                            this.pageNr = 1;
+                            this.buttonText = "Fetch page 1";
+                        }
+                    });
         }
     }
 });
